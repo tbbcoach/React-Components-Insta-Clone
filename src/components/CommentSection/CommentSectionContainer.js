3550ac9d58a616fc818f2e065cee6a0acb4f615c
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-06-24 14:09:40 -0400

    putting project together.

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -3,16 +3,25 @@ import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
+import dummyData from "../../dummy-data";
 
 const CommentSection = props => {
   // Add state for the comments
-
+  const [comments, setComments] = useState(props.comments);
+  console.log('comments here' ,comments);
+  
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
-      <CommentInput />
+      {comments.map((comment) => {
+        return <Comment comment={comment} />
+      })} 
+        
+          <CommentInput />
+          
     </div>
-  );
+  )
 };
+
 
 export default CommentSection;
